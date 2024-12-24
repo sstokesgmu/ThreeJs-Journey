c84114cc9f3bca5fb594fd2326d6898ec1725e86
sstokes <sterlingjstokes@gmail.com>
2024-12-24 14:15:44 -0500

    Completed lesson 5

--- Lesson-05/src/script.js
+++ Lesson-05/src/script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import './style.css'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -36,4 +38,63 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+//renderer.render(scene, camera)
+
+/**
+ * Animate
+ */
+
+// const tick = () => {
+//     //Update objects
+//     mesh.rotation.y += 0.01;
+//     //Reneder
+//     renderer.render(scene, camera);
+    
+//     //Call tick again on the next frame
+//     window.requestAnimationFrame(tick);
+// }
+// tick();
+
+// let time = Date.now();
+// const tick = () => {
+//     const currentTime = Date.now();
+//     const deltaTime = currentTime - time;
+//     time = currentTime; 
+
+//     //Update objects
+//     mesh.rotation.y += 0.002  * deltaTime;
+//     renderer.render(scene,camera);
+//     window.requestAnimationFrame(tick);
+// }
+// tick()
+
+/**
+ * Animating using the clock
+ */
+
+// const clock = new THREE.Clock();
+// const tick = () => {
+//     const elapsedTime = clock.getElapsedTime();
+//     mesh.rotation.y = elapsedTime * 2;
+
+//     //Animating position
+//     mesh.position.x = Math.cos(elapsedTime);
+//     mesh.position.y = Math.sin(elapsedTime);
+
+//     renderer.render(scene, camera);
+//     window.requestAnimationFrame(tick);
+// }
+// tick();
+
+/**
+ * Animating with Gasp
+ */
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+const tick = () => {
+    //Render
+    renderer.render(scene, camera);
+    //Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+}
+tick();
