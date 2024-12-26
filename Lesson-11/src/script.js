e8fabe028371a52960d315918c4f9a2bc41f4d1d
sstokes <sterlingjstokes@gmail.com>
2024-12-26 02:36:47 -0500

    Working Through Lesson 11

--- Lesson-11/src/script.js
+++ Lesson-11/src/script.js
@@ -1,5 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui';
+
+/**
+ * Debug UI
+ */
+const gui = new GUI();
 
 /**
  * Base
@@ -47,6 +53,101 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('./textures/matcaps/4.png')
+const gradientTexture = textureLoader.load('./textures/gradients/5.jpg')
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace;
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+
+
+/**
+ * Objects
+*/
+
+//const material = new THREE.MeshBasicMaterial();
+// material.map = doorHeightTexture;
+// material.color = new THREE.Color("blue");
+// material.wireframe = true;
+// material.transparent = true;
+//material.opacity = .2;
+// material.alphaMap = doorAlphaTexture;
+// material.side = THREE.DoubleSide;
+
+//! Normal Material
+// const material = new THREE.MeshNormalMaterial();
+// material.flatShading = true; 
+
+//! MeshMatcapMaterial
+// const material = new THREE.MeshMatcapMaterial();
+// material.matcap = matcapTexture;
+
+//! MeshDepthMaterial
+//const material = new THREE.MeshDepthMaterial();
+
+//! MeshLambert Material -> Requires that there be light in the scene
+// const material = new THREE.MeshLambertMaterial();
+
+
+//! MeshToonShader
+// const material = new THREE.MeshToonMaterial();
+// //Manually add the gradixent
+// gradientTexture.minFilter = THREE.NearestFilter;
+// gradientTexture.magFilter = THREE.NearestFilter;
+// gradientTexture.generateMipmaps = false;
+// material.gradientMap = gradientTexture;
+
+//!Standard Material
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.45
+material.roughness = 0.65
+gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+/**
+ * Lighting
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff,1);
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+
+scene.add(ambientLight);
+scene.add(pointLight);
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1,1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+
+torus.position.x = 1.5;
+scene.add(sphere, plane, torus);
+
+
+
 /**
  * Renderer
  */
@@ -68,6 +169,15 @@ const tick = () =>
     // Update controls
     controls.update()
 
+    // Update objects
+    sphere.rotation.x = 0.1 * elapsedTime;
+    sphere.rotation.y = 0.1 * elapsedTime;
+    
+    plane.rotation.x = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+
+    torus.rotation.x = 0.1 * elapsedTime;
+    torus.rotation.y = 0.1 * elapsedTime;
     // Render
     renderer.render(scene, camera)
 
@@ -76,3 +186,5 @@ const tick = () =>
 }
 
 tick()
+
+
