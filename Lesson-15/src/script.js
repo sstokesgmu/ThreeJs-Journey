c13b9e82b355db4772b9dffd468c2917c1eab871
sstokes <sterlingjstokes@gmail.com>
2025-02-09 10:57:51 -0500

    Added a new shadow texture that follows the position of the sphere, and completed Lesson 15

--- Lesson-15/src/script.js
+++ Lesson-15/src/script.js
@@ -17,8 +17,12 @@ const scene = new THREE.Scene();
 //Textures
 
 const textureLoader = new THREE.TextureLoader();
-const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
-bakedShadow.colorSpace = THREE.SRGBColorSpace;
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg');
+
+
+
+// const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
+// bakedShadow.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Lights
@@ -129,6 +133,8 @@ const sphere = new THREE.Mesh(
     material
 )
 
+
+
 //!Step 2: decide how the geometry interacts with shadows 
 //! Now there are no other geometries above the sphere, so it can only cast shadows
 
@@ -136,16 +142,29 @@ sphere.castShadow = true;
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    new THREE.MeshBasicMaterial({
-        map: bakedShadow
-    })
+    material
 )
+
+
 plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.7
+plane.position.y = - 0.5
 
 plane.receiveShadow = true;
 
-scene.add(sphere, plane)
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5,1.5),
+    new THREE.MeshBasicMaterial({
+        color:0x000000,
+        transparent: true,
+        alphaMap: simpleShadow,
+    })
+)
+
+sphereShadow.rotation.x = -Math.PI * 0.5;
+sphereShadow.position.y = plane.position.y + 0.01;
+
+
+scene.add(sphere, sphereShadow, plane)
 
 /**
  * Sizes
@@ -203,6 +222,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update the sphere position
+    sphere.position.x = Math.cos(elapsedTime) * 1.5;
+    sphere.position.z = Math.sin(elapsedTime) * 1.5;
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3));
+
+    //Update the shadow
+    sphereShadow.position.x = sphere.position.x;
+    sphereShadow.position.z = sphere.position.z;
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3;
 
     // Update controls
     controls.update()
